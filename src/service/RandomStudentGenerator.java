1ffe414f3924e076ce830d0da291cbd26912c6aa
mordim3r <valent1n.bazylev@gmai.com>
2026-08-20 15:49:38 +0300

    init

--- src/service/RandomStudentGenerator.java
+++ src/service/RandomStudentGenerator.java
@@ -1,4 +1,16 @@
 package service;
 
+import java.util.Random;
+
 public class RandomStudentGenerator {
+    //TODO Эти диапазоны должны совпадать с будущей валидацией Builder
+    private static final int MIN_GROUP = 1;
+    private static final int MAX_GROUP = 100;
+    private static final int MIN_GRADE = 1;
+    private static final int MAX_GRADE = 10;
+    private static final int MIN_RECORD_BOOK = 1;
+    private static final int MAX_RECORD_BOOK = 1000000;
+
+    private final Random random = new Random();
+
 }
