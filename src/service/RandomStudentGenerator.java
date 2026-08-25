ce96d6bf5e8257e7c449f902fea1e1c77965347d
kotlink <vovazimov23@gmail.com>
2026-08-25 14:40:33 +0300

    Add StudentCountService

--- src/service/RandomStudentGenerator.java
+++ src/service/RandomStudentGenerator.java
@@ -6,7 +6,6 @@ import java.util.Random;
 
 import static constans.Constants.*;
 
-
 public class RandomStudentGenerator implements FillStrategy {
     //TODO Эти диапазоны должны совпадать с будущей валидацией Builder
 
