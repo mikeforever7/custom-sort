2bab418eff6dcfa16936cb5cbbfaf3a1887a2d6a
mordim3r <valent1n.bazylev@gmai.com>
2026-08-27 12:37:32 +0300

    add: user input validation

--- src/service/ManualStudentInput.java
+++ src/service/ManualStudentInput.java
@@ -13,16 +13,48 @@ public class ManualStudentInput implements FillStrategy {
         double grade;
         int recordBook;
 
-        System.out.println("Введите номер группы");
-        group = scanner.nextInt();
-        System.out.println("Введите средний балл");
-        grade = scanner.nextDouble();
-        System.out.println("Введите номер зачетки");
-        recordBook = scanner.nextInt();
-        return new Student.Builder()
-                .groupNumber(group)
-                .averageGrade(grade)
-                .recordBookNumber(recordBook)
-                .build();
+        while (true) {
+            System.out.println("Введите номер группы (1-100)");
+            if (scanner.hasNextInt()) {
+                group = scanner.nextInt();
+                if (group < 1 || group > 100) {
+                    System.out.println("Ошибка! " + group + " это не допустимый номер группы. Введите еще раз.");
+                    continue;
+                }
+                break;
+            } else {
+                String badInput = scanner.next();
+                System.out.println("Ошибка! " + badInput + " это не допустимый номер группы. Введите еще раз.");
+            }
+        }
+        while (true) {
+            System.out.println("Введите средний балл (0-10)");
+            if (scanner.hasNextDouble()) {
+                grade = scanner.nextDouble();
+                if (grade < 0 || grade > 10) {
+                    System.out.println("Ошибка! " + grade + " это не допустимый средний балл. Введите еще раз.");
+                    continue;
+                }
+                break;
+            } else {
+                String badInput = scanner.next();
+                System.out.println("Ошибка! " + badInput + " это не допустимый средний балл. Введите еще раз.");
+            }
+        }
+        while (true) {
+            System.out.println("Введите номер зачетки (1-1000000)");
+            if (scanner.hasNextInt()) {
+                recordBook = scanner.nextInt();
+                if (recordBook < 1 || recordBook > 1000000) {
+                    System.out.println("Ошибка! " + recordBook + " это не допустимый номер зачетки. Введите еще раз.");
+                    continue;
+                }
+                break;
+            } else {
+                String badInput = scanner.next();
+                System.out.println("Ошибка! " + badInput + " это не допустимый номер зачетки. Введите еще раз.");
+            }
+        }
+        return new Student.Builder().groupNumber(group).averageGrade(grade).recordBookNumber(recordBook).build();
     }
 }
--- src/service/StudentInputService.java
+++ src/service/StudentInputService.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-
 public class StudentInputService {
 
     public List<Student> inputStudents() {
@@ -22,6 +21,12 @@ public class StudentInputService {
             System.out.println("3- из файла");
             System.out.println("0- выйти");
 
+            if (!scanner.hasNextInt()) {
+                String badInput = scanner.next();
+                System.out.println("Неверный ввод! \"" + badInput + "\" это не целое число. Введите число от 0 до 3");
+                continue;
+            }
+
             int choice = scanner.nextInt();
 
             if (choice == 0) {
@@ -31,38 +36,61 @@ public class StudentInputService {
 
             switch (choice) {
                 case 1:
-                    System.out.println("Введите количество студентов, которых нужно заполнить вручную");
-                    int manualCount = scanner.nextInt();
-                    FillStrategy manualStrategy = new ManualStudentInput();
+                    int manualCount;
+                    while (true) {
+                        System.out.println("Введите количество студентов, которых нужно заполнить вручную");
+                        if (scanner.hasNextInt()) {
+                            manualCount = scanner.nextInt();
+                            if (manualCount <= 0) {
+                                System.out.println("Количество должно быть больше 0. Введите еще раз.");
+                                continue;
+                            }
+                            break;
+                        } else {
+                            String badInput = scanner.next();
+                            System.out.println("Ошибка!" + badInput + "это не целое число. Введите еще раз.");
+                        }
+                    }
 
-                    List<Student> manualStream = Stream.iterate(0, i -> i + 1)
-                            .limit(manualCount)
-                            .map(i -> manualStrategy.fill())
-                            .peek(student -> studentList.add(studentList.size(), student))
-                            .collect(Collectors.toCollection(AwesomeArrayList::new));
+                    FillStrategy manualStrategy = new ManualStudentInput();
+                    List<Student> manualStream = Stream.iterate(0, i -> i + 1).limit(manualCount).map(i -> manualStrategy.fill()).peek(student -> studentList.add(studentList.size(), student)).collect(Collectors.toCollection(AwesomeArrayList::new));
 
                     manualStream.forEach(System.out::println);
                     break;
-                case 2:
-                    System.out.println("Введите количество студентов, которых нужно заполнить случайно");
-                    int randomCount = scanner.nextInt();
-                    FillStrategy randomStrategy = new RandomStudentGenerator();
 
-                    List<Student> randomStream = Stream.generate(() -> randomStrategy.fill())
-                            .limit(randomCount)
-                            .peek(student -> studentList.add(studentList.size(), student))
-                            .collect(Collectors.toCollection(AwesomeArrayList::new));
+                case 2:
+                    int randomCount;
+                    while (true) {
+                        System.out.println("Введите количество студентов, которых нужно заполнить случайно");
+                        if (scanner.hasNextInt()) {
+                            randomCount = scanner.nextInt();
+                            if (randomCount <= 0) {
+                                System.out.println("Количество должно быть больше 0. Введите еще раз.");
+                                continue;
+                            }
+                            break;
+                        } else {
+                            String badInput = scanner.next();
+                            System.out.println("Ошибка!" + badInput + "это не целое число. Введите еще раз.");
+                        }
+                    }
+
+                    FillStrategy randomStrategy = new RandomStudentGenerator();
+                    List<Student> randomStream = Stream.generate(() -> randomStrategy.fill()).limit(randomCount).peek(student -> studentList.add(studentList.size(), student)).collect(Collectors.toCollection(AwesomeArrayList::new));
 
                     randomStream.forEach(System.out::println);
                     break;
+
                 case 3:
-                    //TODO здесь должен быть вызов чтения из файла
+                    // TODO здесь должен быть вызов чтения из файла
                     System.out.println("Чтение из файла будет реализовано позже");
                     break;
+
                 default:
                     System.out.println("Неверный ввод!");
             }
         }
+
         System.out.println("Всего студентов в списке: " + studentList.size());
         return studentList;
     }
