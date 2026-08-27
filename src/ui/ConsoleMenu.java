2abf6df3d40f445e4686c12978a691790ca64c47
Ilya <mikeforever7@yandex.ru>
2026-08-27 21:52:23 +0300

    refactoring console

--- src/ui/ConsoleMenu.java
+++ src/ui/ConsoleMenu.java
@@ -19,7 +19,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-
 public class ConsoleMenu {
     private final Scanner scanner;
     private final SortService sortService;
