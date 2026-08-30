89dc175920661d5140219437c7f6ee8dbfd3ce01
Алехин Максим <alehin200204@yandex.ru>
2026-08-30 10:48:25 +0300

    метод shouldReadStudentsFromFile

--- test/service/StudentInputServiceTest.java
+++ test/service/StudentInputServiceTest.java
@@ -2,6 +2,7 @@ package service;
 
 import model.Student;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
@@ -13,13 +14,19 @@ class StudentInputServiceTest {
 
     private final InputStream originalIn = System.in;
 
-    private final StudentInputService service = new StudentInputService(null);
+    private StudentInputService service;
+    private StudentFileService fileService;
+
+    @BeforeEach
+    void setUp(){
+        fileService = new StudentFileService();
+        service = new StudentInputService(fileService);
+    }
 
     @AfterEach
     void restoreSystemIn() {
         System.setIn(originalIn);
     }
-
 
     @Test
     void shouldGenerateRandomStudents(){
@@ -27,17 +34,38 @@ class StudentInputServiceTest {
 
         assertNotNull(students);
         assertEquals(2, students.size());
-
-
     }
 
-//
-//    @Test
-//    void shouldReadStudentsFromFile() {
-//        List<Student> students = service.inputFromFileStudents("valid.csv");
-//
-//        assertNotNull(students);
-//        assertEquals(2, students.size());
-//    }
+    @Test
+    void shouldReadStudentsFromFile() {
 
+        Student student1 = new Student.Builder()
+                .groupNumber(10)
+                .averageGrade(5.5)
+                .recordBookNumber(1001)
+                .build();
+
+        Student student2 = new Student.Builder()
+                .groupNumber(15)
+                .averageGrade(8.0)
+                .recordBookNumber(1002)
+                .build();
+
+
+        StudentFileService fakeFileService = new StudentFileService(){
+            @Override
+            public List<Student> readStudents(String filePath){
+                return List.of(student1, student2);
+            }
+        };
+
+
+        StudentInputService serviceWithFake = new StudentInputService(fakeFileService);
+
+        //и все потому Student приватный
+        List<Student> students = serviceWithFake.inputFromFileStudents("valid.csv");
+
+        assertNotNull(students);
+        assertEquals(2, students.size());
+    }
 }
